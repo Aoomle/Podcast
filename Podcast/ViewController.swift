b6b93b759809f42f486347b72c4e717d75f572fb
Abdulmalik Muhammad <bawasuru@gmail.com>
2020-05-21 06:55:00 -0700

    Initial Commit

---
+++ Podcast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Podcast
+//
+//  Created by Abdulmalik Muhammad on 21/05/2020.
+//  Copyright © 2020 aoomle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
